7951fd9b8250b6c7647b7b5d121126aac26f6783
Markin Igor <sepahimmelen@gmail.com>
2017-06-14 19:08:33 +0300

    Added webpack

---
+++ test/components/app_test.js
@@ -0,0 +1,14 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+// Use 'describe' do group together similar tests
+describe('App'() => {
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertion' about the target
+    expect(component).to.contain('Simple React and Redux starter');
+  })
+})
